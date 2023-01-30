460dd4052b3a29ce7a657fab53bc39c7d6780353
Ilya <l0stt.ttime@gmail.com>
2023-01-30 11:29:22 +0300

    ifelse

---
+++ example005_ConsoleIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.Write(username);
+    Console.WriteLine(" Приходи ко мне и полетели");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
