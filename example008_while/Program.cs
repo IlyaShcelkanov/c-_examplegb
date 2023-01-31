1791a25b65bf089cf24a1745e144eae3f8e59d88
Ilya <l0stt.ttime@gmail.com>
2023-01-31 09:39:06 +0300

    example001-010

--- example008_while/Program.cs
+++ example008_while/Program.cs
@@ -5,15 +5,23 @@
     if(arg3 > result) result = arg3;
     return result;
 }
-int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int a1 = 24;
+int b1 = 25;
+int c1 = 11;
+int a2 = 13;
+int b2 = 166;
+int c2 = 12;
+int a3 = 11111;
+int b3 = 46;
+int c3 = 44;
 //int max1 = Max(a1, b1, c1);
 //int max2 = Max(a2, b2, c2);
 //int max3 = Max(a3, b3, c3);
 //int max = Max(max1, max2, max3);
 
-int max = Max(Max(array[0], array[1], array[2]),
-    Max(array[3], array[4], array[5]),
-    Max(array[6], array[7], array[8]));
+int max = Max(Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 
 Console.WriteLine(max);
